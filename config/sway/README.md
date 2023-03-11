54a8adb9d2f365b988e6dfa02de8112fab0bcac5
choge <choge.2nd@gmail.com>
2023-03-11 15:45:39 +0900

    Removed sway-related configs to clean things up

--- config/sway/README.md
+++
@@ -1,6 +0,0 @@
-Sway
-====
-
-[Sway](https://github.com/swaywm/sway) is a window manager that runs on Wayland.
-
-This directory contains a few configuration files for Sway.
--- install-admin.sh
+++
@@ -1,5 +0,0 @@
-## Copy some scripts for greetd
-sudo mkdir -p /usr/local/{bin,share/wayland-sessions}
-sudo cp usr-local/bin/* /usr/local/bin/
-sudo cp usr-local/share/wayland-sessions/sway.desktop /usr/local/share/wayland-sessions/
-sudo cp etc/greetd/* /etc/greetd/
