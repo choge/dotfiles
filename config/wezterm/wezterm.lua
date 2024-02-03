2aaf5ed04abe308e27ebe1a5f46a3eacf10e2a30
choge <choge.2nd@gmail.com>
2024-02-04 06:48:46 +0900

    allow overriding configurations

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,12 +1,68 @@
 local wezterm = require 'wezterm'
 
-return {
+function detect_os()
+    local os = io.popen('uname -s'):read('*l')
+    if os == 'Darwin' then
+        return 'macos'
+    elseif os == 'Linux' then
+        return 'linux'
+    else
+        return 'windows'
+    end
+end
+
+function detect_host()
+    local host = io.popen('hostname'):read('*l')
+    return host
+end
+
+function merge_tables(t1, t2)
+    for k,v in pairs(t2) do
+        if (type(v) == "table") and (type(t1[k] or false) == "table") then
+            merge_tables(t1[k], t2[k])
+        else
+            t1[k] = v
+        end
+    end
+    return t1
+end
+
+local osname = detect_os()
+local hostname = detect_host()
+
+-- ------------------------------------------------------------
+-- Base configuration
+-- ------------------------------------------------------------
+local base_config = {
     window_background_opacity = 0.9,
     color_scheme ='Atelierdune (dark) (terminal.sexy)',
 
-    font = wezterm.font {
-        family = 'Monaspace Argon Var',
-        harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
-    },
+    font = wezterm.font({
+        -- family='Monaspace Neon',
+        family='Monaspace Argon',
+        -- family='Monaspace Xenon',
+        -- family='Monaspace Radon',
+        -- family='Monaspace Krypton',
+        weight = "Book",
+        style  = "Normal",
+        harfbuzz_features = {'calt', 'liga', 'dlig', 'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08'},
+    }),
     font_size = 14.0
 }
+
+
+-- ------------------------------------------------------------
+-- Configurations specific to OS and host
+-- ------------------------------------------------------------
+local os_specific = {}
+
+local host_specific = {
+    ['ser6'] = {
+        font_size = 13.0
+    }
+}
+
+local config = merge_tables(base_config, os_specific[osname] or {})
+config = merge_tables(config, host_specific[hostname] or {})
+return config
+
